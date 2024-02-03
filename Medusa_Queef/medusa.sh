1ff1103390520839d5d9239c0b517da66a303106
assume-breach <76174163+assume-breach@users.noreply.github.com>
2024-02-03 12:00:35 -0500

    Update medusa.sh

--- Medusa_Queef/medusa.sh
+++ Medusa_Queef/medusa.sh
@@ -26,7 +26,9 @@ ___  ___         _                 _
      \_/\_\\__,_|\___|\___|_|                       
 
  An homage to Sektor7's Perun's Fart
+  
   credit: @SEKTOR7net @MalDevAcademy
+  
           by assume-breach                         
 EOF
 echo ""
